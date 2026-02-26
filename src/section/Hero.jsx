02d427ca7ab4c3512ed23f7c69b20e0c6244949c
Neketsu <Artates12345@gmail.com>
2026-02-26 18:37:51 +0800

    Added a background

--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -1,3 +1,18 @@
 export const Hero = () => {
-    return <section></section>
-}
+  return (
+    <section className="relative min-h-screen flex items-center overflow-hidden">
+      {/* Background */}
+      <div className="absolute inset-0">
+        <img
+          src="/hero-bg-a.jpg"
+          alt="Hero image"
+          className="w-full h-full object-cover opacity-40"
+        />
+        <div className="absolute inset-0 bg-linear-to-b from-background/30 via-background/20 to-background/75"/>
+      </div>
+
+      {/* Green Dots */}
+      
+    </section>
+  );
+};
